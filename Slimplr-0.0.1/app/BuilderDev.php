cb10501db7ffac4c9c4dfa5abf1b59db140864dd
Robin Schmitz <rob@rs3d.de>
2012-08-05 13:27:53 +0200

    Fix/Workaround for SCRIPT_NAME and slim 1.6.4

--- Slimplr-0.0.1/app/BuilderDev.php
+++ Slimplr-0.0.1/app/BuilderDev.php
@@ -2,6 +2,12 @@
 
 include ('functions.php');
 include ('Slimpr.php');
+
+#show ($_SERVER['SCRIPT_NAME']);
+// Workaround/Bugfix for SLIM 1.6.4
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+#show ($_SERVER['SCRIPT_NAME']);
+
 $config = array(
 	'xml-navigation-pub' => '/routes/application.xml',
 	#'xml-navigation-pub' => '/routes/navigation-pub.xml',
